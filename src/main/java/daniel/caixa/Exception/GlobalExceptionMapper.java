71b56155e41c6d9fbcda8f27207590adef6f5cb4
Daniel <dejbarreto@gmail.com>
2025-08-02 22:45:15 -0300

    Alteração do arquivo README.md
    Signed-off-by: Daniel <dejbarreto@gmail.com>

--- src/main/java/daniel/caixa/Exception/GlobalExceptionMapper.java
+++ src/main/java/daniel/caixa/Exception/GlobalExceptionMapper.java
@@ -1,8 +1,6 @@
 package daniel.caixa.Exception;
 
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
